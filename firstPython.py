f2f70b3620aa2f88d6c1729e0721f9dd1ccefe94
chenshunliang <chenshunliang@yiche.com>
2015-12-07 17:40:01 +0800

    对象

--- firstPython.py
+++ firstPython.py
@@ -1,3 +1,29 @@
-print(1 / 2)
+import sys
+
+
+# print(sys.path)
+# print(1 / 2)
+
 
 # git test
+
+class Student(object):
+    __slots__ = ('name', 'score', 'age')
+
+    def __int__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s : %s' % (self.name, self.score))
+
+
+stu = Student()
+stu.name = 'chen'
+stu.score = 95
+stu.print_score()
+
+stu.age = 10
+print(stu.age)
+# stu.abc = 'abc'
+# print(stu.abc)
